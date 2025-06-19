939bedb1e57739fa96d153eaddba2d3d2019a2fa
Jocsan Rodriguez <jasub270@gmail.com>
2025-06-18 23:08:08 -0400

    saving

--- bonus-script.js
+++ bonus-script.js
@@ -1,8 +1,17 @@
 console.log("hello from JS");
 
-const root = document.getElementById("root");
-const h1 = document.createElement("h1");
-h1.innerText =
-  "Hello! This is my HTML website created entirely from JavaScript";
-h1.style.fontFamily = "Georgia, serif";
-root.appendChild(h1);
+// const root = document.getElementById("root");
+// const h1 = document.createElement("h1");
+// // h1.innerText =
+//   "Hello! This is my HTML website created entirely from JavaScript";
+// h1.style.fontFamily = "Georgia, serif";
+// root.appendChild(h1);
+//jocsan
+const container = document.getElementById("root");
+const h2 = document.createElement("h2");
+h2.innerText = 
+" Hello my name is Jocsan Rodriguez, and here are somethings about me: ";
+// h2.style.animtion = "fadeIn 2s ease-in-out";
+container.appendChild(h2);
+
+
